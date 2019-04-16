4b3e8ad9d3c6290059810046cd49774f6ded0e4b
itomasaki <l1sum@ito.local>
2019-04-16 23:31:09 +0900

    fix

--- setupKobuki.py
+++ setupKobuki.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python3
 
-#with open("/etc/udev/rules.d/57-kobuki.rules", "w") as f:
-with open("57-kobuki.rules", "w") as f:
+with open("/etc/udev/rules.d/57-kobuki.rules", "w") as f:
     f.write('# On precise, for some reason, USER and GROUP are getting ignored.\n')
     f.write('# So setting mode = 0666 for now.\n')
     f.write('SUBSYSTEM=="tty", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", ATTRS{serial}=="kobuki*", MODE:="0666", GROUP:="dialout", SYMLINK+="kobuki"\n')
